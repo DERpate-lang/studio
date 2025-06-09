0fa7bc91082a4a04e7d10eaddcc6683c84edf905
DERpate-lang <mstfyahmdalbyly@gmail.com>
2025-06-10 02:07:58 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
-module.exports = {
-  output: 'export',
-}
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone', // مهم جدًا لـ SSR على Vercel
+  experimental: {
+    serverActions: true, // لو بتستخدم Server Actions
+  },
+};
+
+module.exports = nextConfig;
